874d7d27152613b33e2f7b1f3e1ef6b49045b836
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2022-12-21 15:07:55 -0500

    simplify Publisher class

--- src/ttkbootstrap/publisher.py
+++ src/ttkbootstrap/publisher.py
@@ -1,118 +1,58 @@
-from enum import Enum
-from typing import List
-
-
-class Channel(Enum):
-    """A grouping for Publisher subscribers. Indicates whether the
-    widget is a legacy `STD` tk widget or a styled `TTK` widget.
-
-    Attributes:
-
-        STD (1):
-            Legacy tkinter widgets.
-
-        TTK (2):
-            Themed tkinter widgets.
-    """
-
-    STD = 1
-    TTK = 2
-
-
-class Subscriber:
-    """A subcriber data class used to store information about a specific
-    subcriber to the `Publisher`."""
-
-    def __init__(self, name, func, channel):
-        """Create a subscriber.
-
-        Parameters:
-
-            name (str):
-                The name of the subscriber
-
-            func (Callable):
-                The function to call when messaging.
-
-            channel (Channel):
-                The subscription channel.
-        """
-        self.name = name
-        self.func = func
-        self.channel = channel
+from typing import Dict, Callable, Any, Set
 
 
 class Publisher:
-    """A class used to publish events for widget updates for theme changes
-    or configurations"""
 
-    __subscribers = {}
+    _subscribers: Dict[str, Set[Callable]] = {}
 
     @staticmethod
-    def subscriber_count():
-        return len(Publisher.__subscribers)
+    def add(event, callback):
+        """Subscribe to an event
+
+        Parameters
+        ----------
+        event : str
+            The event name
+
+        callback : Callable
+            A function to call
+        """
+        if event not in Publisher._subscribers:
+            Publisher._subscribers[event] = {callback}
+        else:
+            Publisher._subscribers[event].add(callback)
 
     @staticmethod
-    def subscribe(name, func, channel):
-        """Subscribe to an event.
+    def get(event):
+        """Get a set of subscribers for this event.
 
-        Parameters:
+        Parameters
+        ----------
+        event : str
+            An event name.
 
-            name (str):
-                The widget's tkinter/tcl name.
-
-            func (Callable):
-                A function to call when passing a message.
-
-            channel (Channel):
-                Indicates the channel grouping the subscribers.
+        Returns
+        -------
+        set
+            A set of callbacks
         """
-        subs = Publisher.__subscribers
-        subs[name] = Subscriber(name, func, channel)
+        return Publisher._subscribers.get(event, set())
 
     @staticmethod
-    def unsubscribe(name):
-        """Remove a subscriber
+    def dispatch(event, message=None):
+        """Dispatch an event with an optional message passed to the callback.
 
-        Parameters:
+        Parameters
+        ----------
+        event : str
+            The event name.
 
-            name (str):
-                The widget's tkinter/tcl name.
+        message : Dict[str, Any]
+            A message passed to the callback.
         """
-        subs = Publisher.__subscribers
-        try:
-            del subs[str(name)]
-        except:
-            pass
-
-    def get_subscribers(channel):
-        """Return a list of subscribers
-
-        Returns:
-
-            List:
-                List of key-value tuples
-        """
-        subs = Publisher.__subscribers.values()
-        channel_subs = [s for s in subs if s.channel == channel]
-        return channel_subs
-
-    def publish_message(channel, *args):
-        """Publish a message to all subscribers
-
-        Parameters:
-
-            channel (Channel):
-                The name of the channel to subscribe.
-
-            **args:
-                optional arguments to pass to the subscribers.
-        """
-        subs: List[Subscriber] = Publisher.get_subscribers(channel)
-        for sub in subs:
-            sub.func(*args)
-
-    @staticmethod
-    def clear_subscribers():
-        """Reset all subscriptions."""
-        Publisher.__subscribers.clear()
+        subs = Publisher.get(event)
+        for callback in subs:
+            if message is None:
+                callback()
+            else:
+                callback(message)
