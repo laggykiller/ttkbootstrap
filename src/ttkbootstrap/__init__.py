61bdca62f704157c6e4456884f3336df25cbca53
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2022-12-22 19:38:14 -0500

    remove legacy bootstyle class reference

--- src/ttkbootstrap/__init__.py
+++ src/ttkbootstrap/__init__.py
@@ -1,6 +1,5 @@
 from ttkbootstrap.utils import utility, colorutils
 from ttkbootstrap.style.style import Style
-from ttkbootstrap.style.style import Bootstyle
 from ttkbootstrap.widgets import *
 from ttkbootstrap.window import Window, Toplevel
 
@@ -8,5 +7,3 @@ from tkinter.scrolledtext import ScrolledText
 from tkinter import Variable, StringVar, IntVar, BooleanVar, DoubleVar
 from tkinter import Canvas, Menu, Text
 from tkinter import PhotoImage
-
-Bootstyle.setup_ttkbootstap_api()
